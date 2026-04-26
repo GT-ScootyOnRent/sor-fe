ccffb2330a2cbb4f4d94154e0b58cd75ba76c8f5
Prathamesh Ugale <prathameshrajeugale@gmail.com>
2026-04-26 19:45:59 +0530

    feat: add splash screen before home page (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import { AppRoutes } from './routes';
-import './index.css';
-import { Toaster } from 'sonner';
+import React, { useEffect, useState } from "react";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { AppRoutes } from "./routes";
+import "./index.css";
+import { Toaster } from "sonner";
+import { AnimatePresence } from "framer-motion";
+import SplashScreen from "./components/SplashScreen";
+
+const SESSION_KEY = "splash_seen";
 
 const App: React.FC = () => {
+  const [showSplash, setShowSplash] = useState(false);
+
+  useEffect(() => {
+    const alreadySeen = sessionStorage.getItem(SESSION_KEY);
+
+    if (!alreadySeen) {
+      setShowSplash(true);
+
+      const timer = setTimeout(() => {
+        setShowSplash(false);
+        sessionStorage.setItem(SESSION_KEY, "true");
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, []);
+
   return (
     <Provider store={store}>
-      <Toaster position="top-right" richColors expand={false} duration={4000} closeButton />
-      <AppRoutes />
+      <Toaster
+        position="top-right"
+        richColors
+        expand={false}
+        duration={4000}
+        closeButton
+      />
+
+      <AnimatePresence mode="wait">
+        {showSplash ? <SplashScreen key="splash" /> : <AppRoutes key="app" />}
+      </AnimatePresence>
     </Provider>
   );
 };
